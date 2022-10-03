b2ead202acd5409391d32d4422ebd91758b25411
Hanna <90796659+HannaTylna@users.noreply.github.com>
2022-10-03 22:26:32 +0200

    create function saveMessage

---
+++ packages/backend/src/services/messages-service.ts
@@ -0,0 +1,16 @@
+import MessageItem from "@chat-setInt-app/shared";
+import {
+    saveMessageItem,
+    loadAllMessageItems,
+} from "../models/messages-repository";
+
+export const saveMessage = async (
+    messageItem: MessageItem
+): Promise<MessageItem[]> => {
+    if (!messageItem.text || messageItem.text == "") {
+        throw new Error("Write something!");
+    }
+    messageItem.timeStamp = new Date();
+    await saveMessageItem(messageItem);
+    return await loadAllMessageItems();
+};
