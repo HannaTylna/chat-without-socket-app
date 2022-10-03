1ff6e0dfdfdd2e6f9470105f28280ef5f5eb9fcd
Hanna <90796659+HannaTylna@users.noreply.github.com>
2022-10-03 22:27:42 +0200

    create functions to load and save messages

--- packages/backend/src/models/messages-repository.ts
+++ packages/backend/src/models/messages-repository.ts
@@ -1,6 +1,4 @@
 import MessageItem from "@chat-setInt-app/shared";
-import ChatItem from "@chat-setInt-app/shared";
-
 import { model, Schema, connect } from "mongoose";
 
 const MessageSchema = new Schema({
@@ -12,4 +10,15 @@ const MessageModel = model<MessageItem>("MessageItem", MessageSchema);
 
 export const setupMongoDb = async (url: string) => {
     await connect(url);
+};
+
+export const loadAllMessageItems = async (): Promise<MessageItem[]> => {
+    return MessageModel.find({}).exec();
+};
+
+export const saveMessageItem = async (
+    messageItem: MessageItem
+): Promise<void> => {
+    const newModel = new MessageModel(messageItem);
+    newModel.save();
 };
