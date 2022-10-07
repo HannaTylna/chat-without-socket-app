52868a0ee492d72534890f84962282551d4700e5
Hanna <90796659+HannaTylna@users.noreply.github.com>
2022-10-07 23:20:12 +0200

    add create user router

--- packages/backend/src/routers/user-router.ts
+++ packages/backend/src/routers/user-router.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response } from "express";
+import { createUser } from "../models/users-repository";
 
 const UserRouter = express.Router();
+
+UserRouter.post("/", createUser);
 
 export default UserRouter;
