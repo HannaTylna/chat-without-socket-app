c5e36ea980fac617f7f4b7b331c708266ccd5c88
Hanna <90796659+HannaTylna@users.noreply.github.com>
2022-10-11 18:27:48 +0200

    work auth function

--- packages/backend/src/middlewares/auth.ts
+++ packages/backend/src/middlewares/auth.ts
@@ -1,20 +1,17 @@
 import { NextFunction, Request, Response } from "express";
-import jwt, { Secret, JwtPayload } from "jsonwebtoken";
+import jwt, { Secret } from "jsonwebtoken";
 
 export const secret: Secret = process.env.JWT_TOKEN || "your_jwt_secret";
 
-export interface CustomRequest extends Request {
-    token: string | JwtPayload;
-}
-
 export const auth = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const token = req.header("Authorization")?.replace("Bearer", "");
+        const token = req.headers.authorization!.split(" ")[1];
         if (!token) {
             throw new Error();
         }
         const decoded = jwt.verify(token, secret);
-        (req as CustomRequest).token = decoded;
+        req.body.user = decoded;
+        console.log(decoded);
         next();
     } catch (err) {
         res.status(401).send("Please authenticate!");
