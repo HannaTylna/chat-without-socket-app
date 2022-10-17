3dd2a3e13bcdf9bbbbf09b18cb76747852ac3797
Hanna <90796659+HannaTylna@users.noreply.github.com>
2022-10-17 22:46:10 +0200

    clear

--- packages/backend/src/middlewares/auth.ts
+++ packages/backend/src/middlewares/auth.ts
@@ -7,11 +7,11 @@ export const auth = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const token = req.headers.authorization!.split(" ")[1];
         if (!token) {
-            throw new Error();
+            throw new Error("bad token");
         }
         const decoded = jwt.verify(token, secret);
         req.body.user = decoded;
-        console.log(decoded);
+
         next();
     } catch (err) {
         res.status(401).send("Please authenticate!");
