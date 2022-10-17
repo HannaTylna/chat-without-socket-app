d9a998ea11c20dcd26a34091b1b003ad39764a0e
Hanna <90796659+HannaTylna@users.noreply.github.com>
2022-10-17 21:43:18 +0200

    change usermodel

--- packages/backend/src/models/users-repository.ts
+++ packages/backend/src/models/users-repository.ts
@@ -15,22 +15,21 @@ const User = new Schema({
         type: String,
         require: true,
     },
-    isAdmin: {
-        type: Boolean,
-        default: false,
-    },
-    status: {
-        type: String,
-        default: "offline",
-    },
+    // isAdmin: {
+    //     type: Boolean,
+    //     default: false,
+    // },
+    // status: {
+    //     type: String,
+    //     default: "offline",
+    // },
 });
 
-const UserModel = model<User>("User", User);
+export const UserModel = mongoose.model<User>("User", User);
 
-export const saveUser = async (user: User): Promise<void> => {
+export const createUser = async (user: User): Promise<void> => {
     const newUser = new UserModel(user);
     const salt = await bcrypt.genSalt(10);
     newUser.password = await bcrypt.hash(user.password, salt);
     newUser.save();
 };
-export default UserModel;
